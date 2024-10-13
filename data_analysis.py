f1d5a10eb41f89f17c26637c34c038d3868ca22c
neochara <neochara@umich.edu>
2024-10-13 13:16:34 -0700

    incorporated initial LLM query

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openai import OpenAI
 
 STOCK_LIST =  ['NVDA', 'TSLA', 'AMZN', 'MSFT', 'COST']
 BASE_PATH = '/Users/neo_chara/Desktop/Projects/Stock-Watch'
@@ -22,16 +23,51 @@ def get_description():
     
     return description_dict
 
+def _filter_description(descr_dict):
+    """Filters description dict to keep indices of interest and Close column"""
+    indices_to_keep = ["mean", "min", "max", "std"]
+    characteristic_to_keep = "Close"
 
-def _get_llm_response(df):
-    return "llm response"
+    #restricted_data = {key: df.loc[indices_to_keep] for key, df in descr_dict.items()}
+    restricted_dict = {}
+    for key, df in descr_dict.items():
+        restricted_dict[key] = df[characteristic_to_keep].loc[indices_to_keep]
+    
+    return restricted_dict
+
+
+def _get_llm_response(idx_queried, idx_descr_df):
+    client = OpenAI()
+    df_str = idx_descr_df.to_csv(index=True)
+
+    query_str = f"""
+    I am providing you with the following summary statistics \n{df_str}\n of the {idx_queried.upper()} stock index. 
+    Can you give me a description, just like the following:
+    Here are the statistics for the {idx_queried.upper()} index. The average stock value was $XXX. The min and max were respectively
+    $XX and $XX, and we observed a standard deviation of $XXX.
+    """
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a financial analyst."},
+            {
+                "role": "user",
+                "content": query_str
+            }
+        ]
+    )
+    return completion.choices[0].message.content
+
 
 def get_nl_summary():
-    description_dict = get_description()
+    """"Gets a summary of the filtered descriptions, for each stock index"""
+    descr_dict = get_description()
+    filtered_descr_dict = _filter_description(descr_dict)
 
     response_dict = {}
-    for stock_idx, stock_description in description_dict.items():
-        response_dict[stock_idx] = _get_llm_response(stock_description)
+    for stock_idx, stock_description in filtered_descr_dict.items():
+        response_dict[stock_idx] = _get_llm_response(stock_idx,stock_description)
     
     return response_dict
 
