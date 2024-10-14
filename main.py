23c4a6f65c8b41fb956caa5b9dbb58c7452a44b3
neochara <neochara@umich.edu>
2024-10-13 20:42:58 -0700

    removed old streamlit

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-import yfinance as yf
 import streamlit as st
+from data_analysis import get_clean_data, get_description, get_nl_summary
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-# Set the title for the Streamlit app
-st.title('Trend for This Month')
+st.title('Stock Watch :sunglasses:')
 
-# Define the stock symbol
-stock_symbol = 'NVDA'
+# idx_of_interest = 'cost'
+idx_of_interest = st.selectbox("Select a stock index:",
+                                ('NVDA', 'TSLA', 'AMZN', 'MSFT', 'COST'))
 
-# Get the current date and the first day of the current month
-today = datetime.today()
-#start_of_month = today.replace(day=1)
-start_of_month = today.replace(month=1, day=1)  # start of year
+st.write(f"Let us summarize the {idx_of_interest} stock")
+idx_of_interest = idx_of_interest.lower()
 
-# Download stock data for this month
-nvidia_stock = yf.download(stock_symbol, start=start_of_month, end=today.strftime('%Y-%m-%d'))
+# - - - Plot timeseries - - -
+stocks_dfs_dict = get_clean_data()
 
-# Display the raw data in Streamlit
-st.write("NVIDIA Stock Data for This Month")
-st.dataframe(nvidia_stock)
+idx_df = stocks_dfs_dict[idx_of_interest]
 
-# Plot the closing prices using Matplotlib
-st.write("Closing Price Trend")
+# Display data to streamlit
+st.header(f"{idx_of_interest.upper()} Stock Data")
+st.dataframe(idx_df)
+
+# Plot closing price
+st.header(f"Closing Price Time Series of {idx_of_interest.upper()}")
 
 plt.figure(figsize=(10, 6))
-plt.plot(nvidia_stock.index, nvidia_stock['Close'], marker='o', linestyle='-', color='b')
-plt.title(f'NVIDIA Stock Closing Prices - {today.strftime("%B %Y")}', fontsize=16)
+plt.plot(idx_df.index, idx_df['Close'], marker='o', linestyle='-', color='b')
+plt.title(f'{idx_of_interest.upper()} Stock Closing Prices', fontsize=16)
 plt.xlabel('Date', fontsize=12)
 plt.ylabel('Closing Price (USD)', fontsize=12)
 plt.grid(True)
 st.pyplot(plt)
+
+# - - - Plot timeseries description table - - -
+description_dict = get_description()
+description_idx_df = description_dict[idx_of_interest]
+
+st.header(f"{idx_of_interest.upper()} Stock Data Statistics")
+st.dataframe(description_idx_df)
+
+# - - - Display Text Summary - - -
+# response_dict = get_nl_summary()
+# response_idx = response_dict[idx_of_interest]
+
+# st.header(f"{idx_of_interest.upper()} Stock Summary")
+# st.text(response_idx)
